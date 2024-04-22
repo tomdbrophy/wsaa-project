c5a8083e00d2baa18c1ef0c9bfdd5604ff3e14a0
Tom Brophy <tombrophy@mac.com>
2024-04-22 20:21:22 +0100

    pivoting project

--- server.py
+++ server.py
@@ -7,25 +7,21 @@ app = Flask(__name__, static_url_path='', static_folder='staticpages')
 def index():
     return render_template('index.html')
 
-@app.route('/waveforms', methods=['GET'])
-def get_waveforms():
-    return 'getting all waveforms'
+@app.route('/bookings', methods=['GET'])
+def get_all_bookings():
+    return 'getting all bookings'
 
-@app.route('/waveforms/<waveform>', methods=['GET'])
-def get_ind_waveform(waveform):
-    return f'waveform {waveform} selected'
+@app.route('/bookings/<room>', methods=['GET'])
+def get_room_bookings(room):
+    return f'room {room} selected'
 
-@app.route('/weather', methods=['GET'])
-def get_weather():
-    return 'get weather'
-
-@app.route('/waveforms', methods=['POST'])
+@app.route('/bookings', methods=['POST'])
 def create_waveform():
-    return 'creating waveform'
+    return 'creating booking'
 
-@app.route('/waveforms', methods=['PUT'])
+@app.route('/bookings', methods=['PUT'])
 def update_waveform():
-    return 'update waveform'
+    return 'update booking'
 
 if __name__ == '__main__':
     app.run(debug=True)
