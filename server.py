30f3336fdd8471d12c83288ecab6c2d064ea422c
Tom Brophy <tombrophy@mac.com>
2025-05-16 14:34:08 +0100

    DAO added

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, jsonify, render_template
+from dao import DAO
+from config import db_config
 
-app = Flask(__name__) 
+app = Flask(__name__)
+
+dao = DAO(host=db_config['host'],
+          user=db_config['user'],
+          password=db_config['password'],
+          db=db_config['database'])
 
 
 @app.route('/')
@@ -9,15 +16,22 @@ def index():
 
 @app.route('/activities', methods=['GET'])
 def get_all_activities():
-    return 'getting all activities'
+    # Fetch all activities from the database
+    query = "SELECT * FROM activities"
+    activities = dao.execute_query(query)
+    return jsonify(activities)
 
 @app.route('/staff/<staff_name>', methods=['GET'])
 def staff_hours(staff_name):
-    return f'getting hours for {staff_name}'
+    query = "SELECT * FROM activities WHERE staff_name = %s"
+    hours = dao.execute_query(query, (staff_name,))
+    return jsonify(hours)
 
 @app.route('/modules/<module_name>', methods=['GET'])
 def module_allocation(module_name):
-    return f'getting all allocations for {module_name}'
+    query = "SELECT * FROM activities WHERE module_name = %s"
+    allocations = dao.execute_query(query, (module_name,))
+    return jsonify(allocations)
 
 @app.route('/activities', methods=['POST'])
 def create_activity():
